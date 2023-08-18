d6b82c0c0de7cb7b6bf32bbd0ac4f2865ed3e8cb
Viktor Nagy <viktor.nagy@365.bank>
2023-08-18 11:06:19 +0200

    refactor: extract compute color function

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ const HEIGHT = 490;
 const _drawPixel = (ctx: CanvasRenderingContext2D) => {
   return (color: number, x: number, y: number) => {
     const r = color;
-    const g = color;
-    const b = color;
-    const a = 255;
+    const g = 200;
+    const b = 200;
+    const a = color;
     ctx.fillStyle = "rgba(" + r + "," + g + "," + b + "," + a / 255 + ")";
     ctx.fillRect(x, y, 1, 1);
   };
@@ -20,6 +20,27 @@ const _map = function (
     ((input - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
 
+const mapX = _map(0, WIDTH, -2, 0.47);
+const mapY = _map(0, HEIGHT, -1.12, 1.12);
+
+const computePixelColor = (px: number, py: number): number => {
+  const x0 = mapX(px);
+  const y0 = mapY(py);
+
+  let x = 0;
+  let y = 0;
+
+  let iteration = 0;
+  let maxIteration = 100;
+  while (x * x + y * y <= 2 * 2 && iteration < maxIteration) {
+    const xTemp = x ** 2 - y ** 2 + x0;
+    y = 2 * x * y + y0;
+    x = xTemp;
+    iteration += 1;
+  }
+  return iteration;
+};
+
 const main = () => {
   let canvas = document.getElementById("canvas") as HTMLCanvasElement;
   let context = canvas.getContext("2d");
@@ -29,27 +50,11 @@ const main = () => {
   context.lineWidth = 1;
 
   const drawPixel = _drawPixel(context);
-  const mapX = _map(0, WIDTH, -2, 0.47);
-  const mapY = _map(0, HEIGHT, -1.12, 1.12);
 
   for (let py = 0; py < HEIGHT; py += 1) {
     for (let px = 0; px < WIDTH; px += 1) {
-      const x0 = mapX(px);
-      const y0 = mapY(py);
-
-      let x = 0;
-      let y = 0;
-
-      let iteration = 0;
-      let maxIteration = 100;
-      while (x * x + y * y <= 2 * 2 && iteration < maxIteration) {
-        const xTemp = x ** 2 - y ** 2 + x0;
-        y = 2 * x * y + y0;
-        x = xTemp;
-        iteration += 1;
-      }
-
-      drawPixel(iteration, px, py);
+      const color = computePixelColor(px, py);
+      drawPixel(color, px, py);
     }
   }
 };
